9fba4f03f6172c8c0e513a00e21ffe096ad9a81d
Zarloxx <alibe092@uottawa.ca>
2022-06-08 06:43:59 -0400

    FINAL

--- utils/scripts/script.js
+++ utils/scripts/script.js
@@ -22,8 +22,8 @@ function registerAppointment() {
       * email:    (str) The email of the customer
       * name:     (str) The name of the customer
     --------------------------------------------------------------------------------------------------------------------*/
-    document.getElementById("myPopup").innerHTML = "<b>votre rendez-vous a été enregistré</b>";
-    /*
+    // document.getElementById("myPopup").innerHTML = "<b>votre rendez-vous a été enregistré</b>";
+    
     // Set appointment variables
     var service = "";
     var employee= "";
@@ -32,13 +32,13 @@ function registerAppointment() {
     var name= "";
 
     // Initiate appointment variables
-    if (document.getElementById("service-data1").value) service = "Coupe de Cheveu";
-    else if (document.getElementById("service-data2").value) service = "Teinture de Cheveu";
-    else if (document.getElementById("service-data3").value) service = "Coupe de Barbe";
+    if (document.getElementById("service-data1").checked) service = "Coupe de Cheveu";
+    else if (document.getElementById("service-data2").checked) service = "Teinture de Cheveu";
+    else if (document.getElementById("service-data3").checked) service = "Coupe de Barbe";
     else service = "null";
-    if (document.getElementById("employee-data1").value) employee = "John";
-    else if (document.getElementById("employee-data2").value) employee = "Mike";
-    else if (document.getElementById("employee-data3").value) employee = "Brad";
+    if (document.getElementById("employee-data1").checked) employee = "John";
+    else if (document.getElementById("employee-data2").checked) employee = "Mike";
+    else if (document.getElementById("employee-data3").checked) employee = "Brad";
     else employee = "null";
     date = String(document.getElementById("date-data").value);
     email = String(document.getElementById("email-input").value);
@@ -50,5 +50,5 @@ function registerAppointment() {
         popup.innerHTML = "<b>"+name+", votre rendez-vous a été enregistré</b><br>Service: "+service+"<br>Employee: "+employee+"<br>Date: "+date;
         
     }
-    */
+    
 }
